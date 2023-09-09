4a77dfe446b20c42799062e587609c9b676949da
Sigurd Tullander <legotull.99@gmail.com>
2023-09-10 00:59:12 +0200

    Finish up `horizontal_line.rs`

--- src/horizontal_line.rs
+++ src/horizontal_line.rs
@@ -1,51 +1,113 @@
-use clap::Parser;
+use std::vec;
+
+use clap::{arg, Parser};
 use colored::Colorize;
 use hsv::hsv_to_rgb;
-use rand::prelude::*;
+use rand::{seq::SliceRandom, thread_rng, Rng};
+// use rand::prelude::*;
 use termion::terminal_size;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
-struct Args {
-    #[arg(short, long, default_value_t = String::from("rainbow"))]
+pub struct HorizontalLineArgs {
+    #[arg(short, long, default_value = "rainbow")]
     theme: String,
 
     #[arg(short, long)]
     width: Option<u16>,
+
+    #[arg(short, long)]
+    zsh_prompt_colors: bool,
+
+    #[arg(short, long, default_value = "-")]
+    character: char,
 }
 
-pub fn horizontal_line(theme: String, width: Option<u16>) -> String {
+pub fn colorize_string(string: String, rgb: (u8, u8, u8), zsh_prompt: bool) -> String {
+    let (r, g, b) = rgb;
+    if zsh_prompt {
+        format!("%F{{#{:02x}{:02x}{:02x}}}{}%f", r, g, b, string)
+    } else {
+        format!("{}", string.truecolor(r, g, b))
+    }
+}
+
+pub fn horizontal_line(args: &HorizontalLineArgs) -> Result<String, String> {
+    let theme = &args.theme;
+    let width = args.width;
+    let zsh_prompt_colors = args.zsh_prompt_colors;
+    let character = args.character;
+
     let (columns, _rows) = if width.is_some() {
         (width.unwrap(), 0)
     } else {
         terminal_size().unwrap()
     };
 
-    let mut line = String::new();
+    // let mut line = String::new();
 
-    let hue_start = rand::random::<f64>() * 360.0;
-    let hue_frequency = 1.0 + rand::random::<f64>() * 1.0;
+    let line = match theme.as_str() {
+        "rainbow" => {
+            let hue_start = thread_rng().gen_range(0.0..360.0);
+            let hue_frequency = thread_rng().gen_range(1.0..2.0);
+            let line = (0..columns)
+                .map(|i| {
+                    let rgb = hsv_to_rgb(
+                        (hue_start + (f64::from(i) / f64::from(columns)) * hue_frequency * 360.0)
+                            % 360.0,
+                        1.0,
+                        1.0,
+                    );
+                    colorize_string(String::from(character), rgb, zsh_prompt_colors)
+                })
+                .collect();
+            line
+        }
+        "white" => {
+            let line = (0..columns).map(|_i| character).collect();
+            line
+        }
+        "taktlaus" => {
+            let colors = {
+                let mut colors = vec![(255, 0, 0), (255, 255, 0), (0, 128, 0), (0, 75, 255)];
+                colors.shuffle(&mut thread_rng());
+                colors
+            };
+            let mut num_undistributable_characters = columns % colors.len() as u16;
+            let line = colors
+                .iter()
+                .map(|rgb| {
+                    let text_length = columns / colors.len() as u16
+                        + if num_undistributable_characters > 0 {
+                            num_undistributable_characters -= 1;
+                            1
+                        } else {
+                            0
+                        };
+                    let text = (0..text_length).map(|_i| character).collect();
+                    colorize_string(text, *rgb, zsh_prompt_colors)
+                })
+                .collect();
+            line
+        }
+        _ => {
+            return Err(format!("theme {} not recognized", theme));
+        }
+    };
 
-    for i in 0..columns {
-        let (r, g, b) = match theme.as_str() {
-            "rainbow" => hsv_to_rgb(
-                (hue_start + (f64::from(i) / f64::from(columns)) * hue_frequency * 360.0) % 360.0,
-                1.0,
-                1.0,
-            ),
-            _ => hsv_to_rgb(0.0, 0.0, 1.0),
-        };
-        // println!("rgb: {}, {}, {}, i: {}", r, g, b, i);
-        line += format!("{}", "-".truecolor(r, g, b)).as_str();
-    }
-
-    line
+    Ok(line)
 }
 
 pub fn main() {
     // let mut input = String::new();
     // stdin().lock().read_to_string(&mut input).unwrap();
-    let args = Args::parse();
+    let args = HorizontalLineArgs::parse();
 
-    println!("{}", horizontal_line(args.theme, args.width));
+    match horizontal_line(&args) {
+        Ok(line) => println!("{}", line),
+        Err(message) => {
+            println!("Could not generate horizontal line:");
+            println!("{}", message);
+        }
+    };
 }
